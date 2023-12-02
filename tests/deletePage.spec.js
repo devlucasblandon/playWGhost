fa540ea427cdf97b2733166517a891609f130f5a
Lucas Blandón P <l.blandon@uniandes.edu.co>
2023-12-01 22:13:59 -0500

    DeletePage

--- tests/deletePage.spec.js
+++ tests/deletePage.spec.js
@@ -1,6 +1,9 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 const path = require('path');
+const caps = {
+    'resolution': '1024x768',  // You can choose any supported resolution as per the selected OS and OS version
+};
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://35.244.81.193/ghost');
@@ -26,12 +29,19 @@ test('has pages', async ({ page }) => {
   await page.getByRole('link', { name: 'Page' }).click();
 });
 
-// Test to check if button New Page works
+// Test to check if button Delete Works
 test('has New page', async ({ page }) => {
     await page.goto('http://35.244.81.193/ghost/#/pages');
     //await page.goto('http://localhost:2368/ghost/#/pages');
     await page.getByRole('link', { name: 'New page' }).click();
-    //await page.fill("data-kg=editor", 'Nueva pagina de prueba');
     await page.getByPlaceholder('Page title').fill('Pagina de prueba para borrar');
+    await page.getByTitle('Settings').click();
     await page.screenshot({ path: 'screenshotNewPageDelete.png' });
+    await page.locator(
+        '//*[@id="entry-controls"]/div/div[2]/div/button/span'
+    ).click();
+    await page.screenshot({ path: 'screenshotDeleteButton.png' });
+    page.on('dialog', dialog => console.log(dialog.message()));
+    await page.getByRole('button',  {name: 'Delete', exact: true }).click();
+    await page.screenshot({ path: 'screenshotDeleteButtonConfirm.png' });
   });
